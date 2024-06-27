8337cf67b6fe3c0aebbb4dfc0d640d3b28a55abb
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2024-06-27 19:08:15 +0200

    encryption on messages added --- rollback if needed

--- pkg/message/message.go
+++ pkg/message/message.go
@@ -3,6 +3,9 @@ package message
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
+
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/util"
 )
 
 // Message codes for DHT operations
@@ -23,25 +26,46 @@ type Message struct {
 	Data []byte
 }
 
+// Key used for encryption and decryption
+var encryptionKey []byte
+
+// SetEncryptionKey sets the encryption key for all messages
+func SetEncryptionKey(key []byte) {
+	encryptionKey = key
+}
+
 func NewMessage(t int, data []byte) *Message {
 	return &Message{Type: t, Data: data}
 }
 
 func (m *Message) Serialize() ([]byte, error) {
+	if encryptionKey == nil {
+		return nil, errors.New("encryption key not set")
+	}
+
+	encryptedData, err := util.Encrypt(m.Data, encryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
 	buf := new(bytes.Buffer)
 	if err := binary.Write(buf, binary.BigEndian, int16(m.Type)); err != nil {
 		return nil, err
 	}
-	if err := binary.Write(buf, binary.BigEndian, int16(len(m.Data))); err != nil {
+	if err := binary.Write(buf, binary.BigEndian, int16(len(encryptedData))); err != nil {
 		return nil, err
 	}
-	if _, err := buf.Write(m.Data); err != nil {
+	if _, err := buf.Write([]byte(encryptedData)); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
 }
 
 func DeserializeMessage(data []byte) (*Message, error) {
+	if encryptionKey == nil {
+		return nil, errors.New("encryption key not set")
+	}
+
 	buf := bytes.NewReader(data)
 	var msgType int16
 	if err := binary.Read(buf, binary.BigEndian, &msgType); err != nil {
@@ -53,10 +77,15 @@ func DeserializeMessage(data []byte) (*Message, error) {
 		return nil, err
 	}
 
-	msgData := make([]byte, dataLen)
-	if _, err := buf.Read(msgData); err != nil {
+	encryptedData := make([]byte, dataLen)
+	if _, err := buf.Read(encryptedData); err != nil {
 		return nil, err
 	}
 
-	return &Message{Type: int(msgType), Data: msgData}, nil
+	decryptedData, err := util.Decrypt(string(encryptedData), encryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Message{Type: int(msgType), Data: decryptedData}, nil
 }
