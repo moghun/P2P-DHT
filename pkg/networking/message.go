c5b7156613da8c13bdd6c3b39426244f88f9548d
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2024-06-27 18:04:59 +0200

    message formatting and message serialization/deserialization added

--- pkg/networking/message.go
+++ pkg/networking/message.go
@@ -1,32 +1,62 @@
 package networking
 
 import (
-    "encoding/json"
+	"bytes"
+	"encoding/binary"
 )
 
-type MessageType int
-
+// Message codes for DHT operations
 const (
-    PUT MessageType = iota + 650
-    GET
-    SUCCESS
-    FAILURE
+	DHT_PUT = iota + 650
+	DHT_GET
+	DHT_SUCCESS
+	DHT_FAILURE
+	DHT_PING
+	DHT_PONG
+	DHT_FIND_NODE
+	DHT_NODE_REPLY
+	DHT_FIND_VALUE
 )
 
 type Message struct {
-    Type          MessageType
-    Key           string
-    Value         []byte
-    Success       bool
-    SenderAddress string // Address of the message sender
+	Type int
+	Data []byte
 }
 
-func EncodeMessage(msg Message) ([]byte, error) {
-    return json.Marshal(msg)
+func NewMessage(t int, data []byte) *Message {
+	return &Message{Type: t, Data: data}
 }
 
-func DecodeMessage(data []byte) (Message, error) {
-    var msg Message
-    err := json.Unmarshal(data, &msg)
-    return msg, err
+func (m *Message) Serialize() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.BigEndian, int16(m.Type)); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.BigEndian, int16(len(m.Data))); err != nil {
+		return nil, err
+	}
+	if _, err := buf.Write(m.Data); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func DeserializeMessage(data []byte) (*Message, error) {
+	buf := bytes.NewReader(data)
+	var msgType int16
+	if err := binary.Read(buf, binary.BigEndian, &msgType); err != nil {
+		return nil, err
+	}
+
+	var dataLen int16
+	if err := binary.Read(buf, binary.BigEndian, &dataLen); err != nil {
+		return nil, err
+	}
+
+	msgData := make([]byte, dataLen)
+	if _, err := buf.Read(msgData); err != nil {
+		return nil, err
+	}
+
+	return &Message{Type: int(msgType), Data: msgData}, nil
 }
