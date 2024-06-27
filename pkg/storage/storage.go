661de2c544c83b8319d70ee5aae4dc76d1eedd36
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2024-06-27 18:46:23 +0200

    multi-threaded storage service added

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,35 +1,88 @@
 package storage
 
 import (
-    "sync"
+	"crypto/sha1"
+	"encoding/hex"
+	"sync"
+	"time"
 )
 
 type Storage struct {
-    mu    sync.RWMutex
-    store map[string][]byte
+	data map[string]*storageItem
+	mu   sync.Mutex
+	ttl  time.Duration
 }
 
-func NewStorage() *Storage {
-    return &Storage{
-        store: make(map[string][]byte),
-    }
+type storageItem struct {
+	value   string
+	expiry  time.Time
+	hash    string
 }
 
-func (s *Storage) Put(key string, value []byte) {
-    s.mu.Lock()
-    defer s.mu.Unlock()
-    s.store[key] = value
+func NewStorage(ttl time.Duration) *Storage {
+	return &Storage{
+		data: make(map[string]*storageItem),
+		ttl:  ttl,
+	}
 }
 
-func (s *Storage) Get(key string) ([]byte, bool) {
-    s.mu.RLock()
-    defer s.mu.RUnlock()
-    value, exists := s.store[key]
-    return value, exists
+func (s *Storage) Put(key, value string, ttl int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	hasher := sha1.New()
+	hasher.Write([]byte(value))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+
+	expiry := time.Now().Add(time.Duration(ttl) * time.Second)
+	s.data[key] = &storageItem{
+		value:   value,
+		expiry:  expiry,
+		hash:    hash,
+	}
 }
 
-func (s *Storage) Delete(key string) {
-    s.mu.Lock()
-    defer s.mu.Unlock()
-    delete(s.store, key)
+func (s *Storage) Get(key string) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	item, exists := s.data[key]
+	if !exists {
+		return ""
+	}
+
+	if time.Now().After(item.expiry) {
+		delete(s.data, key)
+		return ""
+	}
+
+	hasher := sha1.New()
+	hasher.Write([]byte(item.value))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	if hash != item.hash {
+		return ""
+	}
+
+	return item.value
+}
+
+func (s *Storage) cleanupExpired() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for key, item := range s.data {
+		if time.Now().After(item.expiry) {
+			delete(s.data, key)
+		}
+	}
+}
+
+func (s *Storage) StartCleanup(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	go func() {
+		for {
+			<-ticker.C
+			s.cleanupExpired()
+		}
+	}()
 }
