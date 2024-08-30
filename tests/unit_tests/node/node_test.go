43bdf1e7dc7f78af479fe326f9d571ecd69248a4
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2024-08-31 00:15:23 +0200

    node.go unit tests

---
+++ tests/unit_tests/node/node_test.go
@@ -0,0 +1,99 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/node"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/storage"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/util"
+)
+
+// TestNodeGenerateNodeID tests the GenerateNodeID function.
+func TestNodeGenerateNodeID(t *testing.T) {
+	nodeID := node.GenerateNodeID("127.0.0.1", 8080)
+	assert.NotEmpty(t, nodeID)
+}
+
+// TestNewNodeInitialization tests the initialization of a new Node.
+func TestNewNodeInitialization(t *testing.T) {
+    port, err := GetFreePort()
+    assert.NoError(t, err, "Failed to get a free port")
+
+    config := &util.Config{
+        P2PAddress:    fmt.Sprintf("127.0.0.1:%d", port),
+        EncryptionKey: []byte("1234567890123456"),
+    }
+    nodeInstance := node.NewNode(config, 120*time.Second)
+
+    assert.NotNil(t, nodeInstance)
+    assert.Equal(t, "127.0.0.1", nodeInstance.IP)
+    assert.Equal(t, port, nodeInstance.Port)
+    assert.NotNil(t, nodeInstance.Storage)
+    assert.IsType(t, &storage.Storage{}, nodeInstance.Storage)
+}
+
+
+// TestNodePutAndGet tests the Put and Get methods of the Node.
+func TestNodePutAndGet(t *testing.T) {
+	config := &util.Config{
+		P2PAddress:    "127.0.0.1:8080",
+		EncryptionKey: []byte("1234567890123456"),
+	}
+	nodeInstance := node.NewNode(config, 120 * time.Second)
+
+	err := nodeInstance.Put("key1", "value1", 60)
+	assert.NoError(t, err)
+
+	value, err := nodeInstance.Get("key1")
+	assert.NoError(t, err)
+	assert.Equal(t, "value1", value)
+}
+
+func TestNodeStorageTTL(t *testing.T) {
+	config := &util.Config{
+		P2PAddress:    "127.0.0.1:8080",
+		EncryptionKey: []byte("1234567890123456"),
+	}
+	nodeInstance := node.NewNode(config, 1* time.Second)
+
+	err := nodeInstance.Put("key1", "value1", 1)
+	assert.NoError(t, err)
+
+	// Immediately check that the value is there
+	value, err := nodeInstance.Get("key1")
+	assert.NoError(t, err)
+	assert.Equal(t, "value1", value)
+
+	time.Sleep(2 * time.Second) // Wait for the TTL to expire
+
+	// After sleep, the value should be gone
+	value, err = nodeInstance.Get("key1")
+	assert.NoError(t, err)
+	assert.Equal(t, "", value) // Value should be empty after TTL expiration
+}
+
+
+/*
+
+NOT IMPLEMENTED
+
+// TestNodeAddPeer tests the AddPeer method of the Node.
+func TestNodeAddPeer(t *testing.T) {
+	config := &util.Config{
+		P2PAddress:    "127.0.0.1:8080",
+		EncryptionKey: []byte("test_encryption_key"),
+	}
+	nodeInstance := node.NewNode(config)
+
+	nodeID := node.GenerateNodeID("127.0.0.1", 8081)
+	nodeInstance.AddPeer(nodeID, "127.0.0.1", 8081)
+
+	// Assuming GetAllPeers is mocked to return added peers
+	peers := nodeInstance.GetAllPeers()
+	assert.NotNil(t, peers)
+	assert.Equal(t, 1, len(peers))
+}
+*/
