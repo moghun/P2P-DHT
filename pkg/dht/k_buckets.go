b89adbfcb0ca30a963111480daffbb2a6006d6b3
keremkz <94916101+Orekk12@users.noreply.github.com>
2024-09-04 02:39:36 +0200

    Add individual bucket logic

--- pkg/dht/k_buckets.go
+++ pkg/dht/k_buckets.go
@@ -1,8 +1,11 @@
 package dht
 
+import "sync"
+
 // KBucket represents a bucket in the Kademlia routing table.
 type KBucket struct {
 	Nodes []*KNode
+	mutex sync.RWMutex
 }
 
 // KNode represents a node in the DHT.
@@ -21,22 +24,47 @@ func NewKBucket() *KBucket {
 
 // AddNode adds a node to the KBucket.
 func (kb *KBucket) AddNode(node *KNode) {
-	// Mock implementation
+	kb.mutex.Lock()
+	defer kb.mutex.Unlock()
+
+	for i := 0; i < len(kb.Nodes); i++ {
+		if kb.Nodes[i].ID == node.ID {
+			// Node already exists in the bucket - move it to the end
+			kb.Nodes = append(kb.Nodes[:i], kb.Nodes[i+1:]...)
+			kb.Nodes = append(kb.Nodes, node)
+			return
+		}
+	}
+
+	if len(kb.Nodes) < 20 {
+		kb.Nodes = append(kb.Nodes, node)
+	} else {
+		// Replace the first node in the bucket
+		kb.Nodes = append(kb.Nodes[1:], node)
+		//TODO - Liveness check to the oldest node
+	}
 }
 
 // RemoveNode removes a node from the KBucket by ID.
 func (kb *KBucket) RemoveNode(nodeID string) {
-	// Mock implementation
+	kb.mutex.Lock()
+	defer kb.mutex.Unlock()
+
+	for i := 0; i < len(kb.Nodes); i++ {
+		if kb.Nodes[i].ID == nodeID {
+			kb.Nodes = append(kb.Nodes[:i], kb.Nodes[i+1:]...)
+			return
+		}
+	}
 }
 
 // GetNodes returns all nodes in the KBucket.
 func (kb *KBucket) GetNodes() []*KNode {
-	// Mock implementation
 	return kb.Nodes
 }
 
 // Contains checks if a node is in the KBucket.
+// TODO is this needed?
 func (kb *KBucket) Contains(node *KNode) bool {
-	// Mock implementation
 	return false
 }
