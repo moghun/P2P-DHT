e475f7a8277221f2e4758ea459e48a28e1243fea
keremkz <94916101+Orekk12@users.noreply.github.com>
2024-09-08 12:27:34 +0200

    Add dht create store message test

--- tests/unit_tests/dht/dht_test.go
+++ tests/unit_tests/dht/dht_test.go
@@ -33,3 +33,37 @@ func TestStoreToStorage(t *testing.T) {
 	assert.NoError(t, err, "GetFromStorage should not return an error")
 	assert.Equal(t, string(value), retrievedValue, "The value retrieved should match the value stored")
 }
+func TestCreateStoreMessage(t *testing.T) {
+	key := "testkey"
+	value := "testvalue"
+
+	// Initialize a real storage and node for testing
+	store := storage.NewStorage(24*time.Hour, []byte("1234567890abcdef"))
+	dht := dht.NewDHT(24*time.Hour, []byte("1234567890abcdef"), "1", "127.0.0.1", 8080)
+	realNode := &node.Node{
+		IP:      "127.0.0.1",
+		Port:    8080,
+		Storage: store,
+		DHT:     dht,
+	}
+
+	// Create a store message
+	storeMsg, err := realNode.DHT.CreateStoreMessage(key, value)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, storeMsg)
+
+	// Deserialize the store message
+	deserializedMsg, err := message.DeserializeMessage(storeMsg)
+	assert.NoError(t, err)
+	assert.NotNil(t, deserializedMsg)
+	assert.IsType(t, &message.DHTStoreMessage{}, deserializedMsg)
+
+	// Convert deserialized key to a string for comparison
+	storeMessage := deserializedMsg.(*message.DHTStoreMessage)
+	deserializedKey := string(storeMessage.Key[:]) // Convert byte32 array to string
+	trimmedKey := strings.TrimRight(deserializedKey, "\x00")
+
+	// Compare both keys as strings
+	assert.Equal(t, key, trimmedKey)
+}
