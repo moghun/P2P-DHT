8aa6121d96090545c172cbc200639c12921fb39a
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2024-09-08 23:51:20 +0200

    dht storage removed, switched to og storage

--- pkg/dht/dht.go
+++ pkg/dht/dht.go
@@ -2,23 +2,23 @@ package dht
 
 import (
 	"bytes"
-	"crypto/sha1"
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
 	"log"
 	"sync"
 	"time"
 
 	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/message"
-	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/util"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/storage"
+
 )
 
 // DHT represents a Distributed Hash Table.
 type DHT struct {
 	RoutingTable *RoutingTable
-	Storage      *DHTStorage
+	Storage      *storage.Storage
 	Network      *message.Network
+	stopOnce sync.Once
 }
 
 type SuccessMessageResponse struct {
@@ -100,7 +100,7 @@ func Deserialize(data []byte) (*SuccessMessageResponse, error) {
 func NewDHT(ttl time.Duration, encryptionKey []byte, id string, ip string, port int) *DHT {
 	return &DHT{
 		RoutingTable: NewRoutingTable(id),
-		Storage:      NewDHTStorage(ttl, encryptionKey),
+		Storage:      storage.NewStorage(ttl, encryptionKey),
 		Network:      message.NewNetwork(id, ip, port),
 	}
 }
@@ -382,103 +382,13 @@ func (d *DHT) Leave() error {
 	return nil
 }
 
-// DHTStorage is a simple in-memory key-value store with TTL and encryption.
-type DHTStorage struct {
-	data map[string]*storageItem
-	mu   sync.Mutex
-	ttl  time.Duration
-	key  []byte // Encryption key
-}
 
-type storageItem struct {
-	value  string
-	expiry time.Time
-	hash   string
-}
-
-// NewDHTStorage initializes a new DHTStorage with a given TTL and encryption key.
-func NewDHTStorage(ttl time.Duration, key []byte) *DHTStorage {
-	storage := &DHTStorage{
-		data: make(map[string]*storageItem),
-		ttl:  ttl,
-		key:  key,
-	}
-	storage.StartCleanup(ttl)
-	return storage
-}
-
-// Put stores a value with the specified TTL.
-func (s *DHTStorage) Put(key, value string, ttl int) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	encryptedValue, err := util.Encrypt([]byte(value), s.key)
-	if err != nil {
-		log.Printf("Error encrypting value: %v", err)
-		return err
-	}
-
-	hasher := sha1.New()
-	hasher.Write([]byte(encryptedValue))
-	hash := hex.EncodeToString(hasher.Sum(nil))
-
-	expiry := time.Now().Add(time.Duration(ttl) * time.Second)
-	s.data[key] = &storageItem{
-		value:  encryptedValue,
-		expiry: expiry,
-		hash:   hash,
-	}
+// Stop gracefully shuts down the DHT and cleanup routines.
+func (d *DHT) Stop() error {
+	// Ensure the stop is called only once
+	d.stopOnce.Do(func() {
+		d.Storage.StopCleanup()
+		log.Println("DHT node stopped gracefully.")
+	})
 	return nil
-}
-
-// Get retrieves the value associated with the key if it exists and hasn't expired.
-func (s *DHTStorage) Get(key string) (string, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	item, exists := s.data[key]
-	if !exists {
-		return "", nil
-	}
-
-	if time.Now().After(item.expiry) {
-		delete(s.data, key)
-		return "", nil
-	}
-
-	hasher := sha1.New()
-	hasher.Write([]byte(item.value))
-	hash := hex.EncodeToString(hasher.Sum(nil))
-	if hash != item.hash {
-		return "", errors.New("data integrity check failed")
-	}
-
-	decryptedValue, err := util.Decrypt(item.value, s.key)
-	if err != nil {
-		return "", err
-	}
-
-	return string(decryptedValue), nil
-}
-
-// StartCleanup starts a routine that periodically cleans up expired items.
-func (s *DHTStorage) StartCleanup(interval time.Duration) {
-	ticker := time.NewTicker(interval)
-	go func() {
-		for range ticker.C {
-			s.CleanupExpired()
-		}
-	}()
-}
-
-// CleanupExpired removes expired items from the storage.
-func (s *DHTStorage) CleanupExpired() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	for key, item := range s.data {
-		if time.Now().After(item.expiry) {
-			delete(s.data, key)
-		}
-	}
 }
