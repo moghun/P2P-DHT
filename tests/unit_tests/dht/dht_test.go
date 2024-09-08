9cdb39c7a77d92d31af41a1a735d016c9e0f7ebf
keremkz <94916101+Orekk12@users.noreply.github.com>
2024-09-08 12:28:04 +0200

    Add dht send store message test

--- tests/unit_tests/dht/dht_test.go
+++ tests/unit_tests/dht/dht_test.go
@@ -1,13 +1,19 @@
 package tests
 
 import (
+	"fmt"
+	"log"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/api"
 	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/dht"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/message"
 	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/node"
 	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/pkg/storage"
+	"gitlab.lrz.de/netintum/teaching/p2psec_projects_2024/DHT-14/tests"
 )
 
 func TestStoreToStorage(t *testing.T) {
@@ -33,6 +39,7 @@ func TestStoreToStorage(t *testing.T) {
 	assert.NoError(t, err, "GetFromStorage should not return an error")
 	assert.Equal(t, string(value), retrievedValue, "The value retrieved should match the value stored")
 }
+
 func TestGetFromStorage(t *testing.T) {
 	key := [32]byte{}
 	value := []byte("value")
@@ -56,6 +63,87 @@ func TestGetFromStorage(t *testing.T) {
 	assert.NoError(t, err, "GetFromStorage should not return an error")
 	assert.Equal(t, string(value), retrievedValue, "The value retrieved should match the value stored")
 }
+
+func TestSendStoreMessage(t *testing.T) {
+	// Set up the receiver node and its network
+	receiverPort, err := tests.GetFreePort()
+	assert.NoError(t, err, "Failed to get a free port")
+
+	receiverDht := dht.NewDHT(24*time.Hour, []byte("1234567890abcdef"), "2", "127.0.0.1", receiverPort)
+	receiverStore := storage.NewStorage(24*time.Hour, []byte("1234567890abcdef"))
+	receiverNode := &node.Node{
+		IP:      "127.0.0.1",
+		Port:    receiverPort,
+		Storage: receiverStore,
+		DHT:     receiverDht,
+	}
+	network_sender := message.NewNetwork(receiverNode.IP, receiverNode.ID, receiverNode.Port)
+	log.Print("Receiver port:", receiverPort)
+
+	// Start the receiver node's network listening for messages
+	go func() {
+		err := network_sender.StartListening()
+		assert.NoError(t, err)
+		log.Print("Receiver is listening")
+	}()
+
+	// Set up the sender node and its network
+	senderPort, err := tests.GetFreePort()
+	assert.NoError(t, err, "Failed to get a free port")
+	senderDht := dht.NewDHT(24*time.Hour, []byte("1234567890abcdef"), "2", "127.0.0.2", senderPort)
+	senderStore := storage.NewStorage(24*time.Hour, []byte("1234567890abcdef"))
+	senderNode := &node.Node{
+		IP:      "127.0.0.2",
+		Port:    senderPort,
+		Storage: senderStore,
+		DHT:     senderDht,
+	}
+
+	network_receiver := message.NewNetwork(senderNode.IP, senderNode.ID, senderPort)
+	log.Print("Sender port:", senderPort)
+
+	senderDht.Network = network_receiver
+	receiverDht.Network = network_sender
+
+	go func() {
+		err := api.StartServer(receiverNode.IP+":"+fmt.Sprint(receiverPort), receiverNode)
+		assert.NoError(t, err, "Failed to start API server")
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	go func() {
+		err := api.StartServer(senderNode.IP+":"+fmt.Sprint(senderPort), senderNode)
+		assert.NoError(t, err, "Failed to start API server")
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	key := "testkey"
+	value := "testvalue"
+
+	receiverKNode := dht.KNode{
+		ID:   receiverNode.ID,
+		IP:   receiverNode.IP,
+		Port: receiverPort,
+	}
+
+	storeMessageResponse, err := senderDht.SendStoreMessage(key, value, receiverKNode)
+	if err != nil {
+		log.Print("Send store message error")
+	}
+
+	//cast storeMessageResponse to DHTSuccessMessage
+	log.Print("Store message response msg typeXXXXXX:", storeMessageResponse.GetType())
+	assert.Equal(t, message.DHT_SUCCESS, storeMessageResponse.GetType(), "The response message should be a success message")
+
+	// Check if the value exists in the storage
+	retrievedValue, _, err := receiverNode.DHT.GET(key)
+	log.Print("Retrieved value:", retrievedValue)
+	assert.NoError(t, err, "GET should not return an error")
+	assert.Equal(t, value, retrievedValue, "The value retrieved should match the value stored")
+}
+
 func TestCreateStoreMessage(t *testing.T) {
 	key := "testkey"
 	value := "testvalue"
